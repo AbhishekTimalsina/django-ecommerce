93da8dd9dfbb2218eb8e8c4e82b7f21c641b75f4
AbhishekTimalsina <hahaguyz546@gmail.com>
2026-04-19 16:19:00 +0545

    home and details page

--- store/views.py
+++ store/views.py
@@ -1,12 +1,12 @@
 from .models import Book
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
 
 # Create your views here.
-class HomeView(TemplateView):
+class HomeView(ListView):
      model = Book
      template_name = "home.html"
 
-class ProductDetailView(TemplateView):
+class ProductDetailView(DetailView):
      model = Book
      template_name = "details.html"
 
